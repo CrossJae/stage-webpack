6b6df1d588e32affb787214a3b637827999dfd82
crossjas_mac <crossjae@163.com>
2017-12-04 20:01:18 +0800

    添加项目结构

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,22 @@
-var path = require('path');
+const path = require('path');
+
+
+const config = {
+
+}
+
 
 module.exports = {
-  
+  entry : {
+    main : './app/index.js', // app入口
+  },
+  output : {
+    path : path.resolve(__dirname, 'build'), // 输出路径
+    filename : '[name].js?[chunkhash]' // 输出文件名
+  },
+  module : {
+    // loaders
+    rules : []
+  },
+  plugin : []
 }
