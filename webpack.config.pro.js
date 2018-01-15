d4456e938a6f3f1e0e2c1db2304a1b4cbddbc2d1
crossjas_mac <crossjae@163.com>
2018-01-15 11:42:51 +0800

    update pro

--- webpack.config.pro.js
+++ webpack.config.pro.js
@@ -19,7 +19,7 @@ module.exports = {
   output: { // output相当于一套规则，所有入口都必须遵守
     path: path.resolve('./build/' + PROJECTDIR ), // 必须传绝对路径 resolve转换成绝对路径
     publicPath: PROJECTCDN, // 打包生成的cdn地址
-    filename: '[name].js' // 输出文件名
+    filename: '[name].js?[hash:8]' // 输出文件名
   },
   module: { // webpack默认只能对Js打包，其他类型文件需要loader处理
     // loaders
@@ -78,13 +78,13 @@ module.exports = {
       filename: 'index.html', // 输出文件名
       template: path.resolve('./app/' + PROJECTDIR + 'template/index.html'), // 模版
       minify: {
-        // collapseWhitespace: true,
+        collapseWhitespace: true,
         removeComments: true, //移除HTML中的注释
       },
       projectPath: PROJECTCDN, // 替换模版里的项目路径
     }),
     // 单独分离css，有其他可配置参数
-    new ExtractTextPlugin('[name].css'),
+    new ExtractTextPlugin('[name].css?[hash:8]'),
     // 压缩js
     new uglifyJsPlugin({
       compress: {
